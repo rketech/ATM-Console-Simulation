01d56b6962bb704cddb98ca1e3f5f0a2ba329086
rketech <122847077+rketech@users.noreply.github.com>
2026-06-13 12:03:08 +0530

    ATM Console Simulation Version 1.0

---
+++ src/Main.kt
@@ -0,0 +1,54 @@
+/*
+    ----- ATM Console Simulation -------
+        Step 1: ATM Simulation (Console Project)
+            Build it in stages:
+
+            Version 1
+
+                Check Balance
+                Deposit Money
+                Withdraw Money
+                Exit
+
+            Version 2
+
+                PIN Verification
+                Multiple Accounts
+                Transaction History
+
+            Version 3
+
+                Use Classes properly
+                Account class
+                Transaction class
+                sealed class Result for success/error messages
+*/
+
+fun main() {
+    println("=================================")
+    println("      ATM Console Simulation     ")
+    println("=================================")
+
+    var balanceAmount = 5000.0 // Balance can never be null so null safety operator is not required
+    var menuChoice: Int?
+
+    do {
+        println("ATM Menu")
+        println()
+        println("   1. Check Balance")
+        println("   2. Deposit Money")
+        println("   3. Withdraw Money")
+        println("   4. Exit")
+        println()
+        print("Enter choice: ")
+        menuChoice = readLine()?.toInt()
+
+        when (menuChoice) {
+            1 -> println("Current Balance: ${balanceAmount} INR") // If the balance changes later through deposits and withdrawals, this option will automatically show the updated amount.
+            2 -> println("Deposit Money Selected")
+            3 -> println("Withdraw Money Selected")
+            4 -> println("Exiting ATM")
+            else -> println("Invalid Choice")
+        }
+    } while (menuChoice != 4)
+} // Main Function Ends
