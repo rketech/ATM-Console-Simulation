a53159848003f4921d39b44f480f89e3c5e9ec4e
rketech <122847077+rketech@users.noreply.github.com>
2026-06-19 14:17:22 +0530

    Introduced Sealed Class for better error / information handling

--- src/Main.kt
+++ src/Main.kt
@@ -41,7 +41,7 @@ sealed class TransactionResult {
 
     data class Success(
         // Success is a child of TransactionResult.
-        val transaction: Transaction,
+        val transaction: Transaction, // created transaction object inorder to handle duplicate value which is already present in Transaction class
         val updatedBalance: Double,
     ) : TransactionResult()
 
